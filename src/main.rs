06602788b3b3d04cea23e27a70fb1a56a068e4b8
Jack Grigg <thestr4d@gmail.com>
2019-10-06 19:18:11 +1100

    Set up CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use gumdrop::Options;
+
+#[derive(Debug, Options)]
+struct AgeOptions {
+    #[options(free, help = "key files")]
+    keys: Vec<String>,
+
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(help = "generate a new key")]
+    generate: bool,
+
+    #[options(help = "decrypt a file")]
+    decrypt: bool,
+
+    #[options(help = "input file")]
+    input: Option<String>,
+
+    #[options(help = "output file")]
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = AgeOptions::parse_args_default_or_exit();
+
+    if opts.generate {
+        println!("TODO: generate");
+    } else if opts.decrypt {
+        println!("TODO: decrypt");
+    } else {
+        println!("TODO: encrypt");
+    }
 }
