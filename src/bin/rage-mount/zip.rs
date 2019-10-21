fb2115ab40c9b9b8788c6a2f40f432c32a96419a
Jack Grigg <thestr4d@gmail.com>
2019-10-21 23:09:16 +1300

    Add file-reading support to rage-mount

--- src/bin/rage-mount/zip.rs
+++ src/bin/rage-mount/zip.rs
@@ -1,7 +1,7 @@
 use fuse_mt::*;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io;
+use std::io::{self, Read};
 use std::path::{Path, PathBuf};
 use std::sync::Mutex;
 use time::Timespec;
@@ -86,6 +86,7 @@ pub struct AgeZipFs {
     inner: Mutex<ZipArchive<age::StreamReader<File>>>,
     dir_map: HashMap<PathBuf, Vec<DirectoryEntry>>,
     open_dirs: Mutex<(HashMap<u64, PathBuf>, u64)>,
+    open_files: Mutex<(HashMap<u64, usize>, u64)>,
 }
 
 impl AgeZipFs {
@@ -111,6 +112,7 @@ impl AgeZipFs {
             inner: Mutex::new(archive),
             dir_map,
             open_dirs: Mutex::new((HashMap::new(), 0)),
+            open_files: Mutex::new((HashMap::new(), 0)),
         })
     }
 }
@@ -125,13 +127,16 @@ impl FilesystemMT for AgeZipFs {
     fn getattr(&self, _req: RequestInfo, path: &Path, fh: Option<u64>) -> ResultEntry {
         let mut inner = self.inner.lock().unwrap();
         let open_dirs = self.open_dirs.lock().unwrap();
+        let open_files = self.open_files.lock().unwrap();
 
         if let Some(fh) = fh {
             if open_dirs.0.contains_key(&fh) {
                 Ok((TTL, DIR_ATTR))
+            } else if let Some(index) = open_files.0.get(&fh) {
+                let zf = inner.by_index(*index).expect("open_files is correct");
+                Ok((TTL, zipfile_to_fuse(&zf)))
             } else {
-                // TODO: Support open files
-                Err(libc::ENOSYS)
+                Err(libc::EBADF)
             }
         } else if self.dir_map.contains_key(zip_path(path)) {
             Ok((TTL, DIR_ATTR))
@@ -184,5 +189,75 @@ impl FilesystemMT for AgeZipFs {
             namelen: u32::max_value(),
             frsize: 64 * 1024,
         })
+    }
+
+    fn open(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
+        let mut inner = self.inner.lock().unwrap();
+        let mut open_files = self.open_files.lock().unwrap();
+
+        for i in 0..inner.len() {
+            if inner.by_index(i).unwrap().sanitized_name() == zip_path(path) {
+                let fh = open_files.1;
+                open_files.0.insert(fh, i);
+                open_files.1 = open_files.1.wrapping_add(1);
+                return Ok((fh, 0));
+            }
+        }
+
+        Err(libc::ENOENT)
+    }
+
+    fn read(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        fh: u64,
+        offset: u64,
+        size: u32,
+        result: impl FnOnce(Result<&[u8], libc::c_int>),
+    ) {
+        let mut inner = self.inner.lock().unwrap();
+        let open_files = self.open_files.lock().unwrap();
+
+        match open_files.0.get(&fh) {
+            Some(index) => {
+                let mut zf = inner.by_index(*index).expect("open_files is correct");
+                if offset > zf.size() {
+                    result(Err(libc::EINVAL));
+                    return;
+                }
+
+                // Skip to offset
+                let mut buf = vec![];
+                buf.resize(offset as usize, 0);
+                if zf.read_exact(&mut buf).is_err() {
+                    result(Err(libc::EIO));
+                    return;
+                }
+
+                // Read bytes
+                let to_read = usize::min(size as usize, (zf.size() - offset) as usize);
+                buf.resize(to_read, 0);
+                match zf.read_exact(&mut buf) {
+                    Ok(_) => result(Ok(&buf)),
+                    Err(_) => result(Err(libc::EIO)),
+                }
+            }
+            None => result(Err(libc::EBADF)),
+        }
+    }
+
+    fn release(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        fh: u64,
+        _flags: u32,
+        _lock_owner: u64,
+        _flush: bool,
+    ) -> ResultEmpty {
+        let mut open_files = self.open_files.lock().unwrap();
+
+        open_files.0.remove(&fh).map(|_| ()).ok_or(libc::EBADF)
     }
 }
