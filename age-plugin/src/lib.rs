995a0b9978175d8ee5419d3e88ce9381d60f9166
Jack Grigg <thestr4d@gmail.com>
2020-08-16 23:40:34 +0100

    age-plugin: Add age_plugin::print_new_identity helper

--- age-plugin/src/lib.rs
+++ age-plugin/src/lib.rs
@@ -1,7 +1,39 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! This is a helper crate for implementing age plugins.
+
+#![forbid(unsafe_code)]
+// Catch documentation errors caused by code changes.
+#![deny(intra_doc_link_resolution_failure)]
+#![deny(missing_docs)]
+
+// Plugin HRPs are age1[name] and AGE-PLUGIN-[NAME]-
+const PLUGIN_RECIPIENT_PREFIX: &str = "age1";
+const PLUGIN_IDENTITY_PREFIX: &str = "age-plugin-";
+
+/// Prints the newly-created identity and corresponding recipient to standard out.
+///
+/// A "created" time is included in the output, set to the current local time.
+pub fn print_new_identity(plugin_name: &str, identity: &[u8], recipient: &[u8]) {
+    use bech32::ToBase32;
+
+    println!(
+        "# created: {}",
+        chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true)
+    );
+    println!(
+        "# recipient: {}",
+        bech32::encode(
+            &format!("{}{}", PLUGIN_RECIPIENT_PREFIX, plugin_name),
+            recipient.to_base32(),
+        )
+        .expect("HRP is valid")
+    );
+    println!(
+        "{}",
+        bech32::encode(
+            &format!("{}{}-", PLUGIN_IDENTITY_PREFIX, plugin_name),
+            identity.to_base32(),
+        )
+        .expect("HRP is valid")
+        .to_uppercase()
+    );
 }
