9ed7bf22a44690c9e67610cc92363f146fbc8f97
Jack Grigg <thestr4d@gmail.com>
2020-11-04 14:16:11 +0000

    age: Use a wider range of data sizes for throughput benchmarks

--- age/benches/throughput.rs
+++ age/benches/throughput.rs
@@ -8,18 +8,17 @@ const KB: usize = 1024;
 fn bench(c: &mut Criterion<CyclesPerByte>) {
     let recipient = x25519::Identity::generate().to_public();
     let mut group = c.benchmark_group("stream");
+    let buf = vec![0u8; 1024 * KB];
 
-    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 128 * KB] {
-        let buf = vec![0u8; *size];
-
-        group.throughput(Throughput::Bytes(*size as u64));
+    for &size in &[KB, 16 * KB, 64 * KB, 128 * KB, 256 * KB, 500 * KB, 1024 * KB] {
+        group.throughput(Throughput::Bytes(size as u64));
 
         group.bench_function(BenchmarkId::new("encrypt", size), |b| {
             let mut output = Encryptor::with_recipients(vec![Box::new(recipient.clone())])
                 .wrap_output(io::sink())
                 .unwrap();
 
-            b.iter(|| output.write_all(&buf))
+            b.iter(|| output.write_all(&buf[..size]))
         });
     }
 
