81f91581bf7e21075519dc23e4a28b4d201dd784
Jack Grigg <thestr4d@gmail.com>
2021-12-23 17:01:46 +0000

    fuzz: Update fuzzer targets for recent changes

--- fuzz/fuzz_targets/decrypt.rs
+++ fuzz/fuzz_targets/decrypt.rs
@@ -1,16 +1,18 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
+use std::iter;
+
 use age::Decryptor;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(decryptor) = Decryptor::new(data) {
         match decryptor {
             Decryptor::Recipients(d) => {
-                let _ = d.decrypt(&[]);
+                let _ = d.decrypt(iter::empty());
             }
             // Don't pay the cost of scrypt while fuzzing.
-            Decryptor::Passphrase(_) => ()
+            Decryptor::Passphrase(_) => (),
         }
     }
 });
