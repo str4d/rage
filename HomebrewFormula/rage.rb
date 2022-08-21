e0f3c590b68a5ed52e8895a84bfd5f936d5cd30e
str4d <thestr4d@gmail.com>
2022-08-21 15:18:58 +0100

    Fix indentation in Homebrew formula

--- HomebrewFormula/rage.rb
+++ HomebrewFormula/rage.rb
@@ -8,7 +8,7 @@ class Rage < Formula
     depends_on "rust" => :build
 
     def install
-      system "cargo", "install", *std_cargo_args(path: './rage')
+        system "cargo", "install", *std_cargo_args(path: './rage')
     end
 
     test do
