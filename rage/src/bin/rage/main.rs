d39ebfe967b7cc645ecf6bb38f7f4f7bc3a7fc0b
Jack Grigg <thestr4d@gmail.com>
2024-01-16 07:22:40 +0000

    Assert that supported SSH identities can't produce ignored recipients
    We produce an error before this point for unsupported SSH identities,
    and ignored recipients are a subset of these. A panic here would
    indicate a programming error in the `age` crate.

--- rage/src/bin/rage/main.rs
+++ rage/src/bin/rage/main.rs
@@ -39,6 +39,11 @@ macro_rules! warning {
     }};
 }
 
+/// Handles error mapping for the given SSH recipient parser.
+///
+/// Returns `Ok(None)` if the parser finds a parseable value that should be ignored. This
+/// case is for handling SSH recipient types that may occur in files we want to be able to
+/// parse, but that we do not directly support.
 #[cfg(feature = "ssh")]
 fn parse_ssh_recipient<F, G>(
     parser: F,
@@ -174,14 +179,14 @@ fn read_recipients(
                 return Err(error::EncryptError::UnsupportedKey(filename, k))
             }
             Ok(identity) => {
-                if let Some(recipient) = parse_ssh_recipient(
+                let recipient = parse_ssh_recipient(
                     || age::ssh::Recipient::try_from(identity),
                     || Err(error::EncryptError::InvalidRecipient(filename.clone())),
                     &filename,
-                )? {
-                    recipients.push(recipient);
-                    continue;
-                }
+                )?
+                .expect("unsupported identities were already handled");
+                recipients.push(recipient);
+                continue;
             }
             Err(_) => (),
         }
